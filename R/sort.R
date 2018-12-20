92cd0b2050c6b1e96f3708c70142cf1c42f56371
waddella <adrian.waddell@roche.com>
2018-12-20 10:56:16 +0100

    user order and sort for sorting rows within rtables and rtables within a list

--- R/sort.R
+++ R/sort.R
@@ -1,7 +1,8 @@
 
-#' Sort Rows in rtable
+
+#' Sort rrows in rtable
 #'
-#' Sort rows in an rtables based on values derived from values within a row.
+#' Return order of rows in an rtables based on values derived from values within a row.
 #'
 #' @param x rtable object
 #' @param indices of column and cell to access a value within a row that is
@@ -11,6 +12,40 @@
 #'   extract the information based on which the rows are sorted.
 #' @param ... arguments passed on to \code{\link{order}}
 #'
+#'
+#' @return order of rows
+order_rrows <- function(x, indices = c(1, 1), ...) {
+  
+  stopifnot(is(x, "rtable"))
+  
+  vals <- if (is.atomic(indices)) {
+    stopifnot(is.numeric(indices))
+    stopifnot(length(indices) == 2)
+    
+    icol <- indices[1]
+    icell <- indices[2]
+    
+    stopifnot(icell > 0)
+    stopifnot(icol >= 0)
+    
+    if (icol == 0) {
+      vapply(x, function(row) {sum(vapply(row, `[[`, numeric(1), icell))} , numeric(1))
+    } else {
+      vapply(x, function(row) row[[icol]][[icell]], numeric(1))
+    }
+  } else if (is.function(indices)) {
+    vapply(x, indices, numeric(1))
+  } else {
+    stop("index is either an atomic vector of a function")
+  }
+  order(vals, ...)
+}
+
+#' Sort Rows in rtable
+#'
+#' Sort rows in an rtables based on values derived from values within a row.
+#'
+#' @inheritParams order_rrows
 #'
 #' @return object of class rtable
 #'
@@ -37,42 +72,16 @@
 #' sort_rrows(tbl, function(row) row[[2]][3] - row[[1]][1])
 #' 
 sort_rrows <- function(x, indices = c(1, 1), ...) {
-  
-  stopifnot(is(x, "rtable"))
-  
-  vals <- if (is.atomic(indices)) {
-    stopifnot(is.numeric(indices))
-    stopifnot(length(indices) == 2)
-    
-    icol <- indices[1]
-    icell <- indices[2]
-    
-    stopifnot(icell > 0)
-    stopifnot(icol >= 0)
-    
-    if (icol == 0) {
-      vapply(x, function(row) {sum(vapply(row, `[`, numeric(1), icell))} , numeric(1))
-    } else {
-      vapply(x, function(row) row[[icol]][icell], numeric(1))
-    }
-  } else if (is.function(indices)) {
-    vapply(x, indices, numeric(1))
-  } else {
-    stop("index is either an atomic vector of a function")
-  }
-  
-  x[order(vals, ...), ]
+  x[order_rrows(x, indices , ...), ]
 }
 
 
-
-
-#' Sort rtables in Nested Lists
+#' Order of rtables in a list
 #'
-#' Sort rtables based on values within the tables.
+#' Return the order of rtables in a list based on values within the tables.
 #'
 #' @inheritParams sort_rrows
-#' @param x a nested list where the leaves are rtables objects
+#' @param x a list of rtables
 #' @param indices of row, column, and cell to access a value within a row that
 #'   is uused for sorting the rows. If the column index is 0 then a sum of the
 #'   cell values over all columns within the row is taken. If indices is a
@@ -98,60 +107,49 @@ sort_rrows <- function(x, indices = c(1, 1), ...) {
 #'    ),
 #'    rtable(
 #'      header = c("A", "B"),
-#'      rrow("r1", c(9,1), c(9,0,19)),
+#'      rrow("r1", c(1,1), c(100,0,19)),
 #'      rrow("r2", c(5,-1), c(8,1,21)),
 #'      rrow("r3", c(1,3), c(3,1,22))
 #'    )
 #' )
 #'
-#' sort_rtables_tree(tbls, c(1,1,1))
-#' sort_rtables_tree(tbls, c(1,0,1))
+#' order_rtables(tbls, c(1,1,1))
+#' order_rtables(tbls, c(1,0,1))
 #'
 #' 
-sort_rtables_tree <- function(x, indices = c(1, 0, 1), ...) {
+#' @export
+order_rtables <- function(x, indices = c(1,0,1), ...) {
   
-  stopifnot(is.numeric(indices) && length(indices) == 3)
+  stopifnot(is.list(x) && are(x, "rtable"))
   
-  if (is(x, "rtable")) {
-    x
-  } else if (is.list(x)) {
+  vals <- if (is.function(indices)) {
+    vapply(x, indices, numeric(1))
+  } else if (is.atomic(indices) && length(indices) == 3) {
+    irow <- indices[1]
+    icol <- indices[2]
+    icell <- indices[3]
     
-    if (all(vapply(x, is, logical(1), "rtable"))) {
-      
-      vals <- if (is.function(indices)) {
-        
-        vapply(x, indices, numeric(1))
-        
-      } else if (is.atomic(indices)) {
-        
-        irow <- indices[1]
-        icol <- indices[2]
-        icell <- indices[3]
-        
-        stopifnot(irow > 0)
-        stopifnot(icell > 0)
-        stopifnot(icol >= 0)
-        
-        vapply(x, function(tbl) {
-          row <- tbl[[irow]]
-          if (icol == 0) {
-            sum(vapply(row, `[`, numeric(1), icell))
-          } else {
-            row[[icol]][icell]
-          }
-        }, numeric(1))
-      } else {
-        stop("index must either be atomic or a function")
-      }
-      
-      x[order(vals, ...)]
+    if (icol == 0) {
+      vapply(x, function(tbl) {sum(vapply(tbl[[irow]], `[[`, numeric(1), icell))} , numeric(1))
     } else {
-      
-      lapply(x, sort_rtables_tree, index = indices, ...)
-      
+      vapply(x, function(tbl) tbl[[irow]][[icol]][[icell]], numeric(1))
     }
+    
   } else {
-    stop("x needs to be either a list or an rtable")
+    stop("indices needs to be a vector of length 3 or a function")
   }
+  order(vals, ...)
 }
 
+#' Sort rtables within a list
+#' 
+#' Return a sorted list of rtables in a list based on values within the tables.
+#' 
+#' @inheritParams order_rtables
+#' 
+#' @export
+#' 
+#' 
+sort_rtables <- function(x, indices = c(1,0,1), ...) {
+  x[order_rtables(x, indices , ...), ]
+}
