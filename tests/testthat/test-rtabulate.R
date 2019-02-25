2733f62a72ee2a90303f2539659bbb507734a47b
waddella <adrian.waddell@roche.com>
2019-02-25 19:07:48 +0100

    add col_wise_args unit tests for rtabulate

--- tests/testthat/test-rtabulate.R
+++ tests/testthat/test-rtabulate.R
@@ -38,3 +38,52 @@ test_that("rtabulate length tests", {
   
   
 })
+
+
+test_that("rtabulate: col_wise_args argument", {
+  
+  get_elmts <- function(x, i) {
+    stopifnot(is(x, "rtable") && nrow(x) == 1)
+    as.vector(sapply(x[[1]], `[[`, i))
+  }
+  
+  check_row1 <- function(x) {
+    expect_identical(get_elmts(x, 2), c(3, 2, 1))
+    expect_identical(get_elmts(x, 3), LETTERS[1:3])
+  }
+  
+  tbl1 <- rtabulate(1:3, factor(letters[1:3]), function(xi, a, b) {
+    list(xi, a, b)
+  }, col_wise_args = list(a = c(3, 2, 1), b = LETTERS[1:3]), row.name = "-")
+  
+  check_row1(tbl1)
+
+  
+  tbl2 <- rtabulate((1:3)>2, factor(letters[1:3]), function(xi, a, b) {
+    list(xi, a, b)
+  }, col_wise_args = list(a = c(3, 2, 1), b = LETTERS[1:3]), row.name = "-")
+  
+  check_row1(tbl2)
+  
+  check_all_rows <- function(x) {
+    for (i in 1:nrow(x)) {
+      check_row1(x[i, ])
+    }
+  }
+  
+  tbl3 <- rtabulate(factor(letters[1:3]), factor(letters[1:3]), function(xi, a, b) {
+    list(if (length(xi) == 0) "-" else xi, a, b)
+  }, col_wise_args = list(a = c(3, 2, 1), b = LETTERS[1:3]))
+  
+  check_all_rows(tbl3)
+  
+  df <- data.frame(
+    v1 = 1:3, v2 = 3:5
+  )
+  tbl4 <- rtabulate(df, factor(letters[1:3]), factor(LETTERS[1:3]),  function(xi, a, b) {
+    list(nrow(xi), a, b)
+  }, col_wise_args = list(a = c(3, 2, 1), b = LETTERS[1:3]))
+  
+  check_all_rows(tbl4)
+  
+})
