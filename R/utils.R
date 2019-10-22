9f9e2f88939df6a9c678ba83b107c21dc6243780
Slama, Ondrej {MDBA~Basel} <ondrej.slama@roche.com>
2019-10-22 13:30:54 +0200

    renaming (#19)

--- R/utils.R
+++ R/utils.R
@@ -18,15 +18,15 @@ num_all_equal <- function(x, tol = .Machine$double.eps ^ 0.5) {
 all_true <- function(lst, fcn) {
   all(vapply(lst, fcn, TRUE))
 }
-is.logical.single <- function(x) {
+is_logical_single <- function(x) {
   !is.null(x) &&
     is.logical(x) &&
     length(x) == 1 &&
     !is.na(x)
 }
-is.logical.vector_modif <- function(x, min_size = 1) {
+is_logical_vector_modif <- function(x, min_size = 1) {
   !is.null(x) &&
     is.atomic(x) &&
     length(x) >= min_size &&
-    all_true(x, is.logical.single)
+    all_true(x, is_logical_single)
 }
