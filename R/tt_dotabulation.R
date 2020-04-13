baf7b27a117a65b03893a5cd60197193812a5598
Adrian Waddell <adrian.waddell@roche.com>
2020-04-13 21:22:59 +0200

    remove unneeded library calls

--- R/tt_dotabulation.R
+++ R/tt_dotabulation.R
@@ -380,9 +380,6 @@ recursive_applysplit = function( df,
 #' @inheritParams argument_conventions
 #' 
 #' @examples
-#' 
-#' library(magrittr)
-#' 
 #' l <- NULL %>% add_colby_varlevels("ARM") %>% 
 #'     add_analyzed_vars("AGE", afun = function(x) {
 #'       setNames(as.list(fivenum(x)), c("minimum", "lower-hinge", "median", "upper-hinge", "maximum"))
