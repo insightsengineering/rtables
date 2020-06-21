e62e103eacca753954274b2dc45b394c344dcf16
Gabe Becker <gabembecker@gmail.com>
2020-06-20 17:31:23 -0700

    add and document example cdisc datasets. Authorized for release by Adrian.

--- R/argument_conventions.R
+++ R/argument_conventions.R
@@ -120,3 +120,38 @@ compat_args <- function(.lst, FUN, col_by, row_by, row.name, format, indent, col
 #'
 #' @format rds (data.frame)
 "DM"
+
+
+
+#' Simulated CDISC Data for examples
+#'
+#' @format rds (data.frame)
+#' @rdname cdisc_data
+"ex_adsl"
+
+#' @rdname cdisc_data
+"ex_adae"
+
+#' @rdname cdisc_data
+"ex_adaette"
+
+#' @rdname cdisc_data
+"ex_adtte"
+
+#' @rdname cdisc_data
+"ex_adcm"
+
+#' @rdname cdisc_data
+"ex_adlb"
+
+#' @rdname cdisc_data
+"ex_admh"
+
+#' @rdname cdisc_data
+"ex_adqs"
+
+#' @rdname cdisc_data
+"ex_adrs"
+
+#' @rdname cdisc_data
+"ex_advs"
