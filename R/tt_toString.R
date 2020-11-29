6afef86658365f7438819e01b5214a8ca369b23a
Adrian Waddell <adrian@waddell.ch>
2020-11-29 20:43:11 +0100

    fix another documentation issue

--- R/tt_toString.R
+++ R/tt_toString.R
@@ -533,7 +533,7 @@ is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
 #' @param x a character vector
 #' @param indent a vector of length \code{length(x)} with non-negative integers
 #' @param incr non-negative integer: number of spaces per indent level
-#' @param include_newline boolean: should newlines also be indented
+#' @param including_newline boolean: should newlines also be indented
 #' 
 #' @export
 #' 
