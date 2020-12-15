7076ea4f78faa62939d1d6838be5f2c2cacd4245
Adrian Waddell <adrian@waddell.ch>
2020-12-15 21:03:41 +0100

    import quantile from stats

--- R/argument_conventions.R
+++ R/argument_conventions.R
@@ -1,6 +1,6 @@
 ## package imports
 #' @importFrom utils head head.matrix tail tail.matrix
-#' @importFrom stats setNames na.omit prop.test binom.test relevel
+#' @importFrom stats setNames na.omit prop.test binom.test relevel quantile
 #' @importFrom htmltools tags tagList
 #' @importFrom magrittr %>%
 #' @import methods
