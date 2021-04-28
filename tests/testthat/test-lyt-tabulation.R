666e831587639eb7a169ac5e31d8cee13ceda2c7
Gabriel Becker <gabembecker@gmail.com>
2021-04-28 14:09:14 -0700

    Added test that confirms that #164 is implemented.

--- tests/testthat/test-lyt-tabulation.R
+++ tests/testthat/test-lyt-tabulation.R
@@ -735,3 +735,43 @@ test_that("deeply nested column layouts work", {
     tbl <- build_table(lyt, ex_adsl)
     expect_identical(ncol(tbl), 19L)
 })
+
+
+
+test_that("topleft label position works", {
+
+    lyt <- basic_table() %>% split_cols_by("ARM") %>%
+        ## add nested column split on SEX with value lables from gend_label
+        split_cols_by("SEX", "Gender", labels_var = "gend_label") %>%
+        ## No row splits have been introduced, so this adds
+        ## a root split and puts summary content on it labelled Overall (N)
+        ## add_colby_total(label = "All") %>%
+        ##    summarize_row_groups(label = "Overall (N)", format = "(N=xx)") %>%
+        add_colcounts() %>%
+        ## add a new subtable that splits on RACE, value labels from ethn_label
+        split_rows_by("RACE", "Ethnicity", labels_var = "ethn_label", label_pos = "topleft") %>%
+        summarize_row_groups("RACE", label_fstr = "%s (n)") %>%
+        ##
+        ## Add nested row split within Race categories for FACTOR2
+        ## using a split function that excludes level C
+        ## value labels from fac2_label
+        split_rows_by("FACTOR2", "Factor2",
+                      split_fun = remove_split_levels("C"),
+                      labels_var = "fac2_label",
+                      label_pos = "topleft") %>%
+        ## Add count summary within FACTOR2 categories
+        summarize_row_groups("FACTOR2") %>%
+        ## Add analysis/data rows by analyzing AGE variable
+        ## Note afun is a function that returns 2 values in a named list
+        ## this will create 2 data rows
+        analyze("AGE", "Age Analysis", afun = function(x) list(mean = mean(x),
+                                                               median = median(x)),
+                format = "xx.xx")
+
+    tab <- build_table(lyt, rawdat)
+    expect_identical(c("Ethnicity", "  Factor2"),
+                   top_left(tab))
+    expect_identical(14L,
+                   nrow(tab))
+})
+
