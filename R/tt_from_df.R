c26bf07ed78a9f9c3b3357be0a7f22349ab71640
Gabriel Becker <gabembecker@gmail.com>
2021-05-05 16:15:06 -0700

    Create simple df to ElementaryTable function. Closes #189

---
+++ R/tt_from_df.R
@@ -0,0 +1,18 @@
+#' Create ElementaryTable from data.frame
+#' @param df data.frame.
+#' @return an \code{ElementaryTable} object with unnested columns corresponding to
+#' \code{names(df)} and row labels corresponding to \code{row.names(df)}
+#' @examples
+#' df_to_tt(mtcars)
+#' @export
+df_to_tt <- function(df) {
+    colnms <- colnames(df)
+    cinfo <- manual_cols(colnms)
+    rnames <- rownames(df)
+    havern <- !is.null(rnames)
+    kids <- lapply(1:nrow(df), function(i) {
+        rni <- if(havern) rnames[i] else ""
+        do.call(rrow, c(list(row.name = rni), unclass(df[i,])))
+    })
+    ElementaryTable(kids = kids, cinfo = cinfo)
+}
