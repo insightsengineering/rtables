7cb2f14dd8be1f292a4bc234824dc7fa3d8d9f3c
Gabriel Becker <gabembecker@gmail.com>
2021-05-24 17:02:23 -0700

    Additional tests for referential footnotes.

--- tests/testthat/test-header-footer.R
+++ tests/testthat/test-header-footer.R
@@ -19,4 +19,24 @@ test_that("referential footnotes work", {
     expect_identical(rdf[1, "nrowrefs"], 1L)
     expect_identical(rdf[2, "ncellrefs"], 3L)
     expect_identical(rdf[1, "ncellrefs"], 0L)
+
+
+    analysisfun2 <- function(x, cutoff,  ...) {
+        mn <- mean(x)
+        if(mn >= cutoff)
+            cf = list(mean = "Elevated group mean")
+        else
+            cf = list()
+        in_rows(mean = mean(x),
+                range = range(x),
+                .cell_footnotes = cf,
+                .formats = list(mean = "xx.x", range = "xx - xx"))
+    }
+    lyt2 <- basic_table() %>%
+        split_cols_by("ARM") %>%
+        analyze("AGE", afun = analysisfun2, extra_args = list(cutoff = mean(ex_adsl$AGE)))
+    tbl <- build_table(lyt2, ex_adsl)
+    rdf2 <- make_row_df(tbl)
+    expect_identical(rdf2$nrowrefs, c(0L, 0L))
+    expect_identical(rdf2$ncellrefs, c(2L, 0L))
 })
