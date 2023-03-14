b1a29a38a339c6708145659b73b34c748241f29d
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-03-14 17:54:03 -0400

    Update wording after review

--- R/split_funs.R
+++ R/split_funs.R
@@ -79,8 +79,8 @@ setGeneric(".applysplit_ref_vals",
 #' functions and modify either the incoming data before they are called or
 #' their outputs.
 #'
-#' @seealso [make_split_fun()] for a simplified interface for creating custom split 
-#' functions, and [split_funcs] for a variety of pre-defined split functions.
+#' @seealso [make_split_fun()] for the API for creating custom split functions, 
+#' and [split_funcs] for a variety of pre-defined split functions.
 #'
 #' @examples
 #' # Example of a picky split function. The number of values in the column variable
