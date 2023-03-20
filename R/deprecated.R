3c1d47c021b75f1b12c5d7eb81705b2eea2a3363
Emily de la Rua <59304861+edelarua@users.noreply.github.com>
2023-03-20 13:07:20 -0400

    Start deprecation cycle for trim_zero_rows (#585)

--- R/deprecated.R
+++ R/deprecated.R
@@ -20,4 +20,27 @@ add_analyzed_var <- function(lyt, var, label = var, afun,
     split_rows(lyt, spl, pos)
   }
 }
+
+#' Trim Zero Rows
+#'
+#' @param tbl table object
+#'
+#' @return an rtable object
+#'
+#' @export
+trim_zero_rows <- function(tbl) {
+    .Deprecated(
+        new = "prune_table(tbl, all_zero) or prune_table(tbl, prune_zeros_only)",
+        old = "trim_zero_rows(tbl)"
+    )
+    
+    stopifnot(is(tbl, "VTableTree"))
+    
+    rows <- collect_leaves(tbl, TRUE, TRUE)
+    torm <- vapply(rows, function(x) {
+        identical(unname(unlist(row_values(x))), rep(0L, ncol(tbl)))
+    }, NA, USE.NAMES = FALSE)
+    tbl[!torm, , keep_topleft = TRUE]
+    
+}
 # nocov end
