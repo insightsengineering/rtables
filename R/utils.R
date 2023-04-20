0da36be9c8266771e4a26e14def98a2e75bb98b1
Melkiades <davide.garolini@roche.com>
2023-04-20 20:35:10 +0200

    initial test

--- R/utils.R
+++ R/utils.R
@@ -90,7 +90,8 @@ is_logical_vector_modif <- function(x, min_length = 1) {
 #'   analyze(c("AGE"), function(x) {
 #'     in_rows(
 #'       "mean" = rcell(mean(x), align = "right"),
-#'       "sd" = rcell(sd(x), align = "left"), .formats = c("xx.x")
+#'       "sd" = rcell(sd(x), align = "left"), 
+#'       .formats = c("xx.x")
 #'     )
 #'   }, show_labels = "visible", na_str = "NE")
 #'
@@ -101,7 +102,7 @@ is_logical_vector_modif <- function(x, min_length = 1) {
 #'
 #' @export
 rtables_aligns <- function() {
-  c("left", "right", "center")
+  c("left", "right", "center", "decimal", "dec_right", "dec_left")
 }
 
 chk_rtables_align <- function(algn) {
