3be35314fdd8dfdfe78df0f694cc705a7cb7e2d3
Melkiades <davide.garolini@roche.com>
2023-06-09 15:03:58 +0200

    out pruning

--- tests/testthat/test_empty_tables.R
+++ tests/testthat/test_empty_tables.R
@@ -22,23 +22,20 @@ fnotes_at_path(tbl_to_prune, rowpath = c("F")) <- "agoraphobia"
 fnotes_at_path(tbl_to_prune, rowpath = NULL,
                colpath = c("ARM", "A: Drug X")) <- "talassophobia"
 
+pruned_tbl <- tbl_to_prune %>% prune_table
 
 test_that("prune_table produces a degenerate table with decoration 
           if used on empty tbl", {
-
-pruned_tbl <- tbl_to_prune %>% prune_table
-
-expect_identical(top_left(pruned_tbl), top_left(tbl_to_prune))
-expect_identical(names(pruned_tbl), names(tbl_to_prune))
-expect_identical(main_title(pruned_tbl), main_title(tbl_to_prune))
-expect_identical(subtitles(pruned_tbl), subtitles(tbl_to_prune))
-expect_identical(main_footer(pruned_tbl), main_footer(tbl_to_prune))
-expect_identical(prov_footer(pruned_tbl), prov_footer(tbl_to_prune))
-expect_equivalent(matrix_form(pruned_tbl)$ref_footnotes, character(0))
+    expect_identical(top_left(pruned_tbl), top_left(tbl_to_prune))
+    expect_identical(names(pruned_tbl), names(tbl_to_prune))
+    expect_identical(main_title(pruned_tbl), main_title(tbl_to_prune))
+    expect_identical(subtitles(pruned_tbl), subtitles(tbl_to_prune))
+    expect_identical(main_footer(pruned_tbl), main_footer(tbl_to_prune))
+    expect_identical(prov_footer(pruned_tbl), prov_footer(tbl_to_prune))
+    expect_equivalent(matrix_form(pruned_tbl)$ref_footnotes, character(0))
 })
 
 test_that("degenerate table can keep information if subset", {
-    pruned_tbl <- tbl_to_prune %>% prune_table
     subset_tbl <- tbl_to_prune[-c(1:nrow(tbl_to_prune)), ,
                  keep_topleft = TRUE, 
                  keep_titles = TRUE, 
