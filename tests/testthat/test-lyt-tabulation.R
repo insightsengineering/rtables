27955ab1cc4d953f7ec12b38b1a79cce3c35fb71
Melkiades <davide.garolini@roche.com>
2023-06-26 15:06:33 +0200

    why is different??

--- tests/testthat/test-lyt-tabulation.R
+++ tests/testthat/test-lyt-tabulation.R
@@ -931,6 +931,7 @@ test_that(".spl_context works in content and analysis functions", {
 })
 
 test_that(".spl_context contains information about the column split", {
+    ## Duplication hack
     DM_tmp <- DM %>% 
         mutate(method = factor("Mean")) %>% 
         mutate(SEX = factor(SEX))
@@ -952,7 +953,12 @@ test_that(".spl_context contains information about the column split", {
         split_cols_by("SEX") %>%
         analyze(vars = "BMRKR1", afun = analysis_fun_fin, format = "xx.x")
     
-    lyt %>% build_table(DM_tmp)
+    tbl <- lyt %>% build_table(DM_tmp)
+    
+    tol <- 0.02
+    DM_tmp_F <- DM_tmp %>% filter(SEX == "F", STRATA1 == "B")
+    expect_equal(tbl[4, 1, drop = TRUE], mean(DM_tmp_F$BMRKR1), tolerance = tol)
+    expect_equal(tbl[4, 3, drop = TRUE], sd(DM_tmp_F$BMRKR1), tolerance = tol)
 })
 
 test_that(".spl_context contains col information and multivars works", {
