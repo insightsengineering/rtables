08911afd1ebb8268ecf30438ff0b0b77745558cb
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-10-19 17:09:25 -0400

    Fix pipe lint

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -461,7 +461,8 @@ test_that("Cell and column label wrapping works in printing", {
 
 
 test_that("row label indentation is kept even if there are newline characters", {
-  ANL <- DM %>% mutate(value = rnorm(n()), pctdiff = runif(n())) %>%
+  ANL <- DM %>% 
+    mutate(value = rnorm(n()), pctdiff = runif(n())) %>%
     filter(ARM == "A: Drug X")
   ANL$ARM <- factor(ANL$ARM)
 
