3324c1b31fa86fca4e9ab5595b630d0c8fd22648
Melkiades <davide.garolini@roche.com>
2023-11-23 16:36:12 +0100

    fixing docs

--- R/tree_accessors.R
+++ R/tree_accessors.R
@@ -3221,6 +3221,10 @@ setMethod("trailing_section_div<-", "TableRow", function(obj, value) {
 #'   to set the section divider only for sections that are splits or analyses if the number of
 #'   values is less than the number of rows in the table. If `TRUE`, the section divider will
 #'   be set for all the rows of the table.
+#' @param value character. The section divider single character vector. Each character that is
+#'   not `NA_character_` will produce a trailing separator for each row of the table. `value` length
+#'   should reflect the number of rows, or be between 1 and the number of splits/levels. 
+#'   See details for more information.
 #'
 #' @return The section divider string. Each line that does not have a trailing separator
 #'   will have `NA_character_` as section divider.
