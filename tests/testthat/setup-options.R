37f298a3cb4a1be72d90eb959438df0d9c48504a
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-02-01 16:54:35 +0100

    explain opts_partial_match_old

--- tests/testthat/setup-options.R
+++ tests/testthat/setup-options.R
@@ -1,3 +1,6 @@
+# `opts_partial_match_old` is left for exclusions due to partial matching in dependent packages (i.e. not fixable here)
+# it might happen that it is not used right now, but it is left for possible future use
+# use with: `withr::with_options(opts_partial_match_old, { ... })` inside the test
 opts_partial_match_old <- list(
   warnPartialMatchDollar = getOption("warnPartialMatchDollar"),
   warnPartialMatchArgs = getOption("warnPartialMatchArgs"),
