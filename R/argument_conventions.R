8a3a0578ff0482ca516c499c837ec8f0f5b68508
Joe Zhu <sha.joe.zhu@gmail.com>
2025-12-14 19:44:36 +0800

    update

--- R/argument_conventions.R
+++ R/argument_conventions.R
@@ -98,9 +98,13 @@ gen_args <- function(df, alt_counts_df, spl, pos, tt, tr, verbose, colwidths, ob
 #'   functions. See [formatters::list_valid_format_labels()] for a list of all available format strings.
 #' @param format_na_str (`string`)\cr string which should be displayed when formatted if this cell's value(s)
 #'   are all `NA`.
-#' @param formats_var (`string` or `NULL`)\cr `NULL` (the default) or the name of the list column containing named lists of default formats to use. These will be applied with the same precedence as the `format` argument; i.e., they will not override formats (other than `"default"`) set within the afun.
+#' @param formats_var (`string` or `NULL`)\cr `NULL` (the default) or the name of the list column containing named
+#' lists of default formats to use. These will be applied with the same precedence as the `format` argument; i.e.,
+#' they will not override formats (other than `"default"`) set within the afun.
 #' Cannot be used simultaneously with `format`.
-#' @param na_strs_var (`string` or `NULL`)\cr `NULL` (the default) or the name of the list column containing named lists of default NA strings to use. These will be applied with the same precedence as the `format` argument; i.e., they will not override formats (other than `"default"`) set within the afun.
+#' @param na_strs_var (`string` or `NULL`)\cr `NULL` (the default) or the name of the list column containing named
+#' lists of default NA strings to use. These will be applied with the same precedence as the `format` argument; i.e.,
+#' they will not override formats (other than `"default"`) set within the afun.
 #' Cannot be used simultaneously with `format`. Cannot be used if `formats_var` is `NULL`.
 #' @param indent_mod (`numeric`)\cr modifier for the default indent position for the structure created by this
 #'   function (subtable, content table, or row) *and all of that structure's children*. Defaults to 0, which
